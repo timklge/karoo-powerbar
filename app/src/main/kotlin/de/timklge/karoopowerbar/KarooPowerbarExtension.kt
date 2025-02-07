73f5a75d02a01293d3a1fd4b9e91afe4ff7e410e
Tim Kluge <timklge@gmail.com>
2025-02-07 18:48:19 +0100

    Enable minification for release build

--- app/src/main/kotlin/de/timklge/karoopowerbar/KarooPowerbarExtension.kt
+++ app/src/main/kotlin/de/timklge/karoopowerbar/KarooPowerbarExtension.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class KarooPowerbarExtension : KarooExtension("karoo-powerbar", "1.3.3") {
+class KarooPowerbarExtension : KarooExtension("karoo-powerbar", "1.3.4") {
 
     companion object {
         const val TAG = "karoo-powerbar"
