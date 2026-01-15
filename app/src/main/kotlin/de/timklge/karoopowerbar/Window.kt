0648a86761531420d63e38ed6852b5bcaa49785d
Tim Kluge <timklge@gmail.com>
2026-01-15 19:28:33 +0100

    Fix front type, field

--- app/src/main/kotlin/de/timklge/karoopowerbar/Window.kt
+++ app/src/main/kotlin/de/timklge/karoopowerbar/Window.kt
@@ -355,7 +355,7 @@ class Window(
     }
 
     enum class FlightAttendantSuspensionLocation(val dataTypeId: String, val stateFieldId: String) {
-        FRONT("TYPE_SUSPENSION_FRONT_ID", "FIELD_SUSPENSION_FRONT_STATE_ID"),
+        FRONT("TYPE_SUSPENSION_STATE_FRONT_ID", "FIELD_SUSPENSION_STATE_FRONT_ID"),
         REAR("TYPE_SUSPENSION_STATE_REAR_ID", "FIELD_SUSPENSION_STATE_REAR_ID")
     }
 
